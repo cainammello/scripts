78a56cf06a2e5cd74f7ea409dd53126ca05f478c
Cainã Mello <cainammello@gmail.com>
2020-04-27 02:29:48 -0300

    Update install_and_setup_java_8_180_241.sh

--- ubuntu-and-ubuntu-based-distros-post-install/install_and_setup_java_8_180_241.sh
+++ ubuntu-and-ubuntu-based-distros-post-install/install_and_setup_java_8_180_241.sh
@@ -52,4 +52,12 @@ show_header "Checking if everything was successful ..."
 java -version
 javac -version
 
+show_info "Would you like to delete the installation files, folders and other files used in this installation? [y,n]"
+read -n 1 answer
+if [[ $answer =~ ^[Yy]$ ]]; then    
+    sudo rm -r $DIRETORIO_DOWNLOADS && show_success "Everything deleted successful." || show_error "Error to delete some files or folders."
+else
+    show_info "Ok. The folders and files will be kept."
+fi
+
 show_header "Well Done!!! Everything was set up! :)"
